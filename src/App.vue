084eda0f63dae5fedaf6e63189dc542450b58e21
Your Name <nauman_08@hotmail.com>
2020-08-07 19:26:36 +0500

    vue project initiate

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,33 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    <form @submit.prevent="onSubmit">
+      <ValidationProvider name="E-mail" rules="required|email" v-slot="{ errors }">
+        <input v-model="email" type="email">
+        <span>{{ errors[0] }}</span>
+      </ValidationProvider>
+    <button :disabled="invalid" type="submit">Submit Here</button>
+
+  </form>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HelloWorld from './components/HelloWorld.vue';
+import { ValidationProvider } from 'vee-validate';
 export default {
+  data: () => ({
+    email: '',
+  }),
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    ValidationProvider
+  },
+ methods: {
+    onSubmit () {
+      alert('Form has been submitted!');
+    }
   }
 }
 </script>
